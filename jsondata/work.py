b5586763c1cd79976941aafee6911e2b4b036e1b
alexaleluia12 <alexaleluiaforgit@gmail.com>
2018-03-23 08:54:16 -0300

    refatoracao

--- jsondata/work.py
+++ jsondata/work.py
@@ -8,10 +8,18 @@ banco_nome = 'dbtemp'
 
 def get_db_conn():
     conn = pymysql.connect(
-        host='localhost', user='root', password='pl90alex',
+        host='localhost', user='root', password='',
         db=banco_nome
     )
     return conn
+
+def get_many(conn, query):
+    conn.execute(query)
+    return conn.fetchall()
+
+def save_string(content, path_file):
+    with open(path_file, 'w') as hfile:
+        hfile.write(content)
 
 def main():
     query_estados = "SELECT * FROM estados"
@@ -22,33 +30,28 @@ def main():
     try:
         with conn.cursor() as cursor:
             # estados
-            cursor.execute(query_estados)
-            rp = cursor.fetchall()
+            rp = get_many(cursor, query_estados)
             for e in rp:
                 id_, nome, sigla = e
                 elemento = {"id": id_, "nome": nome, "sigla": sigla}
                 estados.append(elemento)
             
             json_estados = json.dumps(estados)
-            with open('../estados.json', 'w') as hestados:
-                hestados.write(json_estados)
+            save_string(json_estados, '../estados.json')
             
             # cidades
-            cursor.execute(query_cidades)
-            rp = cursor.fetchall()
+            rp = get_many(cursor, query_cidades)
             for e in rp:
                 id_, nome, estados_id = e
                 elemento = {"id": id_, "nome": nome, "estados_id": estados_id}
                 cidades.append(elemento)
             
             json_cidades = json.dumps(cidades)
-            with open('../cidades.json', 'w') as hecidades:
-                hecidades.write(json_cidades)
+            save_string(json_cidades, '../cidades.json')
 
     finally:
         conn.close()
 
 if __name__ == '__main__':
     main()
-
 
